10de66219538b95fd7b347a6dd2060bde2a38e97
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-09-20 11:00:12 -0600

    added auto spawning

--- ball-scene/sketch.js
+++ ball-scene/sketch.js
@@ -5,17 +5,12 @@ let ballArray = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  for (let index = 0; index < 30; index++) {
-    let newBall = {
-      x: random(width),
-      y: random(height),
-      radius: random(10, 30),
-      ballColor: color(random(255), random(255), random(255), random(255)),
-      dx: random(5, 10),
-      dy: random(5, 10),
-    };
-    ballArray.push(newBall);    
+  for (let index = 0; index < 1; index++) {
+    spawnBall();
   }
+
+  //spawn a ball every 0.5 seconds
+  window.setInterval(spawnBall, 500);
 }
 
 function draw() {
@@ -23,6 +18,24 @@ function draw() {
 
   moveBall();
   displayBall();
+}
+
+function mousePressed() {
+  spawnBall();
+  ballArray[ballArray.length-1].x = mouseX;
+  ballArray[ballArray.length-1].y = mouseY;
+}
+
+function spawnBall() {
+  let newBall = {
+    x: random(width),
+    y: random(height),
+    radius: random(10, 30),
+    ballColor: color(random(255), random(255), random(255), random(255)),
+    dx: random(5, 10),
+    dy: random(5, 10),
+  };
+  ballArray.push(newBall);
 }
 
 function moveBall() {
