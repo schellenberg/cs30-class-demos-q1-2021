a269e4277a1430eac650b42e4d6c1822d9baa53c
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-09-27 10:58:45 -0600

    first grid demo

---
+++ grid/sketch.js
@@ -0,0 +1,66 @@
+// First Grid Example
+
+let gridSize = 10;
+let grid;
+
+function setup() {
+  // create largest square possible for canvas
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  
+  grid = createRandomGrid(gridSize);
+}
+
+function draw() {
+  background(220);
+
+  displayGrid();
+}
+
+function displayGrid() {
+  let cellSize = width/gridSize;
+
+  for (let y=0; y<grid.length; y++) {
+    for (let x=0; x<grid[y].length; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+
+function createEmptyGrid(howLarge) {
+  let emptyArray = [];
+  for (let y=0; y<howLarge; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<howLarge; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function createRandomGrid(howLarge) {
+  let emptyArray = [];
+  for (let y=0; y<howLarge; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<howLarge; x++) {
+      if (random(0, 100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
+}
